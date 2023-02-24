f96fc532f478e6a74c7f3b277bd717acf80e9680
foxstudent103020 <vlad.tredo86@gmail.com>
2023-02-24 16:31:45 +0200

    Added logger

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,12 @@
+using System.Text;
 using Api.Extensions;
 using Domain.Context;
 using Domain.Repository;
 using Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,24 +22,49 @@ builder.Services.AddScoped<IIncomeExpenseService, IncomeExpenseService>();
 builder.Services.AddScoped<IFinanceOperationService, FinanceOperationService>();
 builder.Services.AddScoped<IDateReportService, DateReportService>();
 
+builder.Services.AddScoped<IUserService, UserService>();
+
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    var key = Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]);
+    o.SaveToken = true;
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+//builder.Services.AddSingleton<IJWT>()
+
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration).Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.AddSerilog(logger);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.ConfigureExceptionHandler();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
